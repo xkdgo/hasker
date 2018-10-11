102e0f37b165f569e13710006ec23b34473ed819
xkdgo <xkdgo@rambler.ru>
2018-10-12 00:25:31 +0500

    added navbar and about page

--- hasker/hasker/config/urls.py
+++ hasker/hasker/config/urls.py
@@ -18,10 +18,12 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/', views.about, name='hasker-about'),
 ]
 
 if settings.DEBUG:
