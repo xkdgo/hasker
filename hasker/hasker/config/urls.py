9621d777cb8e836e6c14505bfd83e9c77e9b9afa
xkdgo <xkdgo@rambler.ru>
2018-10-12 03:05:15 +0500

    added user app auth and profile edit

--- hasker/hasker/config/urls.py
+++ hasker/hasker/config/urls.py
@@ -17,13 +17,20 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
+from users import views as user_views
 from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('profile/', user_views.profile, name='profile'),
     path('about/', views.about, name='hasker-about'),
+    path('', views.home, name='hasker-home'),
 ]
 
 if settings.DEBUG:
--- hasker/hasker/config/views.py
+++ hasker/hasker/config/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def about(request):
     return render(request, 'hasker/about.html', {'title': 'About'})
+
+
+def home(request):
+    return render(request, 'hasker/home.html', {'title': 'Home'})
